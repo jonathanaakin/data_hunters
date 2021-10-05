74ea480b2b91d8c2d5a5241fd6e4e172dee3ce47
Nick Paterno <48722864+npaterno@users.noreply.github.com>
2021-10-05 13:21:43 -0700

    Blizzard correction (#27)
    * new blog file structure
    
    * cleaned data forblizzard_salary

--- data-raw/blizzard_salary/blizzard_salary-dataprep.R
+++ data-raw/blizzard_salary/blizzard_salary-dataprep.R
@@ -11,10 +11,11 @@ raw_data <- read_csv(here::here("data-raw/blizzard_salary/salary.csv"))
 # cleaning: 
 
 blizzard_salary <- raw_data %>% 
-  mutate(current_salary = as.numeric(str_replace(raw_data$current_salary, "[$,]", "")),
+  mutate(current_salary = parse_number(current_salary),
          percent_incr = as.numeric(str_replace(raw_data$percent_incr, "[%]","")),
          location = case_when(location == "LA" ~ "Los Angeles",
-                              TRUE ~ location))
+                              TRUE ~ location),
+         current_title = str_to_title(current_title))
 
 # save -------------------------------------------------------------------------
 
