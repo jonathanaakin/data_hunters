72179aeb4a480741e37557d8e928c0cb372816f2
npaterno <nicholas.paterno@gmail.com>
2021-10-30 11:06:25 -0700

    minor adjustment to lego_sample

--- data-raw/lego_sample/lego_sample-dataprep.R
+++ data-raw/lego_sample/lego_sample-dataprep.R
@@ -1,5 +1,6 @@
 library(readxl)
 library(janitor)
+library(dplyr)
 
 # load data ---------------------------------------------------------------------
 lego_sample <- read_excel(here::here("data-raw/lego_sample/lego_sample.xlsx"))
@@ -9,7 +10,9 @@ lego_sample <- read_excel(here::here("data-raw/lego_sample/lego_sample.xlsx"))
 # cleaning: Making Variables Snake Case
 
 lego_sample <- lego_sample %>% 
-  clean_names()
+  clean_names() %>% 
+  select(-availability)
+
 # save --------------------------------------------------------------------------
 
 usethis::use_data(lego_sample, overwrite = TRUE)
