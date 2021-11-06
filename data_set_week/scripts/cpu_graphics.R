6c2a3a045fc0e8ffb0de6267adb1a6958407d301
Nick Paterno <48722864+npaterno@users.noreply.github.com>
2021-11-05 22:53:10 -0700

    Paterno blog cpu (#32)
    * final race_justice images
    
    * more drafting for cpu analysis
    
    * cpu blog images complete

--- data_set_week/scripts/cpu_graphics.R
+++ data_set_week/scripts/cpu_graphics.R
@@ -4,24 +4,27 @@ library(stringr)
 library(lubridate)
 library(forcats)
 library(tidyr)
+library(openintro)
+library(kableExtra)
 
 #-------------------------------------------------------------------------------
 
-core_i7 <- cpu %>% 
-  filter(str_detect(name, "Core i7")==TRUE) %>% 
-  separate(name, into = c("series", "gen"), sep = "-") %>% 
+core <- cpu %>% 
+  filter(str_detect(name, "Core")==TRUE) %>% 
+  separate(name, into = c("level", "series", "gen")) %>% 
   separate(gen, into = c("model", "suffix"), sep = "(?<=[0-9])(?=[A-Za-z])") %>%
+  filter(series != "M" & series != "m3") %>% 
   mutate(gen = case_when(
-    str_detect(model, "[:digit:]{3}") ~ "First",
-    str_detect(model, "^2") ~ "Second",
-    str_detect(model, "^3") ~ "Third",
-    str_detect(model, "^4") ~ "Fourth",
-    str_detect(model, "^5") ~ "Fifth",
-    str_detect(model, "^6") ~ "Sixth",
-    str_detect(model, "^7") ~ "Seventh",
-    str_detect(model, "^8") ~ "Eighth",
-    str_detect(model, "^9") ~ "Ninth",
-    str_detect(model, "^10") ~ "Tenth"
+    str_detect(model, "^2[0-9]{3}") ~ "Second",
+    str_detect(model, "^3[0-9]{3}") ~ "Third",
+    str_detect(model, "^4[0-9]{3}") ~ "Fourth",
+    str_detect(model, "^5[0-9]{3}") ~ "Fifth",
+    str_detect(model, "^6[0-9]{3}") ~ "Sixth",
+    str_detect(model, "^7[0-9]{3}") ~ "Seventh",
+    str_detect(model, "^8[0-9]{3}") ~ "Eighth",
+    str_detect(model, "^9[0-9]{3}") ~ "Ninth",
+    str_detect(model, "^10[0-9]{2,3}") ~ "Tenth",
+    TRUE ~ "First"
   ),
   platform = case_when(
     str_detect(suffix, "U$") == TRUE ~ "Mobile",
@@ -39,20 +42,63 @@ core_i7 <- cpu %>%
                                   "Sixth", "Seventh", "Eighth", "Ninth","Tenth"
                                   )))
 
+test <- core %>% 
+  group_by(series,gen) %>% 
+  summarize(mean = mean(cores))
+
+ggplot(test, aes(gen, mean, fill = series))+
+  geom_col(position = "dodge")
+
+consumer <- cpu %>% 
+  filter(threads <= 64 & socket != "PCIe x16") %>% 
+  mutate(hyper_threading = case_when(
+    threads > cores ~ "yes",
+    threads == cores ~ "no"
+  ),
+  yr = year(released)) %>% 
+  pivot_longer(cols = c("base_clock", "boost_clock"), names_to = "clock_type", 
+               values_to = "clock_speed")
+
+consumer_base <- consumer %>% 
+  filter(clock_type == "base_clock")
+
+consumer_boost <- consumer %>% 
+  filter(clock_type == "boost_clock")
+
+ggplot()+
+  geom_boxplot(consumer, 
+               mapping = aes(clock_speed, clock_type, color = hyper_threading))+
+  stat_summary(fun = mean, geom = "point", col = "blue")+
+  stat_summary(fun = mean, geom = "text", col = "blue", vjust = -1,
+               aes(label = paste("Mean:", round(..x.., digits = 4))))+
+  labs(
+    title = "Are multithreaded processors faster?",
+    x = "Processor Speed (GHz)", 
+    y = "",
+    color = "Multithreaded?"
+  )+
+  scale_y_discrete(labels = c("Boost clock", "Base clock"))+
+  scale_color_manual(values = c(IMSCOL["pink", "full"], IMSCOL["blue", "full"]), 
+                     labels = c("No", "Yes"))+
+  theme_minimal()
+
+ggsave(here::here("data_set_week/images/cpu_boxplot.png"))
 
 
+# Hyper threading does not affect base clock but it does affect the boost clock!
+# as anova
+summary(aov(clock_speed ~ hyper_threading, data = consumer))
 
+# as t-test
 
+infer::t_test(consumer_base, clock_speed ~ hyper_threading,
+              order = c("yes", "no")) %>% 
+  kbl(linesep = "", booktabs = TRUE, align = "lcccccc", caption = "") %>%
+  kable_styling(bootstrap_options = c("striped", "condensed"),
+                latex_options = c("striped", "hold_position"))
 
-
-
-
-
-
-
-
-
-
-
-
-
+infer::t_test(consumer_boost, clock_speed ~ hyper_threading,
+              order = c("yes", "no"))%>% 
+  kbl(linesep = "", booktabs = TRUE, align = "lcccccc", caption = "") %>%
+  kable_styling(bootstrap_options = c("striped", "condensed"),
+                latex_options = c("striped", "hold_position"))
--- data_set_week/scripts/race_justice_graphics.R
+++ data_set_week/scripts/race_justice_graphics.R
@@ -1,14 +1,15 @@
-  library(kableExtra)
-  library(ggplot2)
-  library(dplyr)
-  library(openintro)
-
+library(kableExtra)
+library(ggplot2)
+library(dplyr)
+library(openintro)
+library(infer)
+  
   # Conditional probabilities of response for each race/ethnicity
   race_justice %>%
    count(race_eth, response) %>%
    group_by(race_eth) %>%
      mutate(prop = n / sum(n)) %>% 
-    kbl(linesep = "", booktabs = TRUE, align = "lcccccc", caption = "") %>%
+    kbl(linesep = "", booktabs = TRUE, align = "lccc", caption = "") %>%
     kable_styling(bootstrap_options = c("striped", "condensed"), 
                   latex_options = c("striped", "hold_position")) 
   
@@ -27,10 +28,31 @@
        )+
      theme_minimal()
 
-   plot(1:7, 7:1, col=IMSCOL, pch=19, cex=6, xlab="", ylab="",
-        xlim=c(0.5,7.5), ylim=c(-2.5,8), axes=FALSE)
-   text(1:7, 7:1+0.7, paste("IMSCOL[", 1:7, "]", sep=""), cex=0.9)
-   points(1:7, 7:1-0.7, col=IMSCOL[,2], pch=19, cex=6)
-   points(1:7, 7:1-1.4, col=IMSCOL[,3], pch=19, cex=6)
-   points(1:7, 7:1-2.1, col=IMSCOL[,4], pch=19, cex=6)   
+   # faceted bar plot of proportions
+   plot_data <- race_justice %>%
+     count(race_eth, response) %>%
+     group_by(race_eth) %>%
+     mutate(prop = n / sum(n))
+   
+   ggplot(plot_data, aes(x = race_eth, y = prop, fill = response)) +
+     geom_col() +
+     labs(
+       x = "Race / ethnicity",
+       y = "Proportion",
+       title = "Do you think Black and White people receive
+   equal treatment from the police?"
+     )+
+     scale_fill_manual(
+       values = c(IMSCOL[1, 1], IMSCOL[2, 1], IMSCOL[2,3])
+     )+
+     theme_minimal()+
+     facet_wrap(~response)+
+     guides(fill = "none")
+   
+   # chi-square test for independence
+ race_justice %>% 
+   observe(response ~ race_eth, stat = "Chisq", null = "independence")
+ 
+ qchisq(0.05, 6, lower.tail=FALSE)
+
    
