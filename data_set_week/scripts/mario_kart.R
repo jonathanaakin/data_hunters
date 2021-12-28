d4b0a5c2ad48d8fe748e97efd92aca72857b2121
npaterno <nicholas.paterno@gmail.com>
2021-12-27 21:59:25 -0800

    a few more images that were not used in the blog post

--- data_set_week/scripts/mario_kart.R
+++ data_set_week/scripts/mario_kart.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(openintro)
 library(dplyr)
+library(broom)
+library(patchwork)
 
 plot_data <- mariokart %>% 
   mutate(diff_pr = total_pr - start_pr - ship_pr)
@@ -15,3 +17,30 @@ ggplot(plot_data, aes(n_bids, diff_pr))+
   )
 
 ggsave(here::here("data_set_week/images/mario_kart_scatter.png"))
+
+mod <- lm(diff_pr ~ n_bids, data = plot_data)
+df <- augment(mod)
+ggplot(df, aes(x = .fitted, y = .resid)) + 
+  geom_point(color = IMSCOL["blue", "full"])+
+  theme_minimal()+
+  labs(
+    title = "",
+    x = "Fitted value", 
+    y = "Residual"
+  )+
+  geom_hline(yintercept = 0)
+
+ggsave(here::here("data_set_week/images/mario_kart_residuals.png"))
+
+ggplot(df, aes(x = .resid))+
+  geom_histogram(binwidth = 10, color = "white", fill = IMSCOL["blue", "full"])+
+  theme_minimal()+
+  labs(
+    title = "",
+    x = "Residual", 
+    y = ""
+  )
+
+ggsave(here::here("data_set_week/images/mario_kart_residuals-histo.png"))
+
+
